00d245691dd54299199004eb47d0fcbf549855ff
Tim <zengtian006@gmail.com>
2017-02-20 22:47:27 +0800

    Test upload

--- app/src/main/java/com/tim/room/fragment/MyAccountFragment.java
+++ app/src/main/java/com/tim/room/fragment/MyAccountFragment.java
@@ -1,7 +1,10 @@
 package com.tim.room.fragment;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.DisplayMetrics;
@@ -16,19 +19,21 @@ import android.widget.TextView;
 
 import com.tim.room.MainActivity;
 import com.tim.room.R;
+import com.tim.room.utils.ImageUtils;
 
 /**
  * Created by Zeng on 2017/1/3.
  */
 
-public class MyAccountFragment extends Fragment {
+public class MyAccountFragment extends Fragment implements ImageUtils.ImageAttachmentListener {
 
     private static final String TAG = MyAccountFragment.class.getSimpleName();
     Button btn_logout;
     TextView tv_name, tv_gender, tv_room_name;
     RelativeLayout relativeLayout;
     Context mContext;
-    ImageView iv2;
+    ImageView iv_user;
+    ImageUtils imageutils;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,6 +46,7 @@ public class MyAccountFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_myaccount, container, false);
         this.mContext = getContext();
+//        imageutils = new ImageUtils((Activity) rootView.getContext());
         findView(rootView);
         serView();
         setListener();
@@ -55,6 +61,13 @@ public class MyAccountFragment extends Fragment {
                 Intent intent = new Intent(mContext, MainActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
+            }
+        });
+
+        iv_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                imageutils.imagepicker(1);
             }
         });
     }
@@ -83,5 +96,11 @@ public class MyAccountFragment extends Fragment {
         tv_gender = (TextView) rootView.findViewById(R.id.tv_gender);
         relativeLayout = (RelativeLayout) rootView.findViewById(R.id.RelativeLayout1);
         btn_logout = (Button) rootView.findViewById(R.id.btn_logout);
+        iv_user = (ImageView) rootView.findViewById(R.id.iv_user);
+    }
+
+    @Override
+    public void image_attachment(int from, String filename, Bitmap file, Uri uri) {
+
     }
 }
