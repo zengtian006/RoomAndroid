2e4157eee786175a68b6c7a4e729c4b1607226ad
Zeng <zengtian006@gmail.com>
2017-02-24 16:09:33 +0800

    Tim

--- app/src/main/java/com/tim/room/activity/ItemFullScreenViewer.java
+++ app/src/main/java/com/tim/room/activity/ItemFullScreenViewer.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -12,27 +13,46 @@ import com.tim.room.R;
 import com.tim.room.adapter.CardAdapter;
 import com.tim.room.helper.CardScaleHelper;
 import com.tim.room.model.Items;
+import com.tim.room.rest.RESTFulService;
+import com.tim.room.rest.RESTFulServiceImp;
 
 import java.util.List;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+
 public class ItemFullScreenViewer extends AppCompatActivity implements CardAdapter.OnFeedItemClickListener {
-    public static List<Items> mItems;
+    private static final String TAG = ItemFullScreenViewer.class.getSimpleName();
+
+    List<Items> mItems;
+    int position;
     private RecyclerView mRecyclerView;
     private CardScaleHelper mCardScaleHelper = null;
+    RESTFulService updateItemService;
+    CardAdapter cardAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_full_screen_viewer);
+
+        updateItemService = RESTFulServiceImp.createService(RESTFulService.class);
+
+
         Intent intent = this.getIntent();
         Bundle bundle = intent.getExtras();
         mItems = (List<Items>) bundle.getSerializable("items");
-        int position = (int) bundle.getSerializable("position");
+        position = (int) bundle.getSerializable("position");
+        setRecyclerView();
 
+    }
+
+    private void setRecyclerView() {
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         mRecyclerView.setLayoutManager(linearLayoutManager);
-        CardAdapter cardAdapter = new CardAdapter(this, mItems);
+        cardAdapter = new CardAdapter(this, mItems);
         cardAdapter.setOnFeedItemClickListener(this);
         mRecyclerView.setAdapter(cardAdapter);
         // mRecyclerView绑定scale效果
@@ -49,6 +69,34 @@ public class ItemFullScreenViewer extends AppCompatActivity implements CardAdapt
     @Override
     public void onMoreClick(View v, int position) {
         Toast.makeText(ItemFullScreenViewer.this, "More click", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPublicClick(View v, int position, boolean b) {
+        if (b) {
+            mItems.get(position).setGlobal("1");
+            updateItemService.updateItem(mItems.get(position)).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<Boolean>() {
+                @Override
+                public void accept(Boolean aBoolean) throws Exception {
+                    Log.v(TAG, "result: " + aBoolean);
+                    if (aBoolean) {
+                        Toast.makeText(ItemFullScreenViewer.this, "This item has been set as public", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        } else {
+            mItems.get(position).setGlobal("0");
+            updateItemService.updateItem(mItems.get(position)).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<Boolean>() {
+                @Override
+                public void accept(Boolean aBoolean) throws Exception {
+                    if (aBoolean) {
+                        Toast.makeText(ItemFullScreenViewer.this, "This item has been set as private", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+//            cardAdapter.notifyDataSetChanged();
+        }
+
     }
 
     @Override
--- app/src/main/java/com/tim/room/adapter/CardAdapter.java
+++ app/src/main/java/com/tim/room/adapter/CardAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Switch;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
@@ -73,6 +74,21 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.ViewHolder> {
                 onFeedItemClickListener.onProfileClick(itemView);
             }
         });
+
+        cellFeedViewHolder.stPublic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onFeedItemClickListener.onPublicClick(itemView, cellFeedViewHolder.getAdapterPosition(), cellFeedViewHolder.stPublic.isChecked());
+            }
+        });
+
+//        cellFeedViewHolder.stPublic.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+//                onFeedItemClickListener.onPublicClick(itemView, cellFeedViewHolder.getAdapterPosition(), b);
+//
+//            }
+//        });
     }
 
     @Override
@@ -84,6 +100,12 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.ViewHolder> {
                 .fitCenter()
                 .crossFade()
                 .diskCacheStrategy(DiskCacheStrategy.ALL).into(holder.imageView);
+        if (item.getGlobal().equals("1")) {//public item
+            holder.stPublic.setChecked(true);
+        } else {
+            holder.stPublic.setChecked(false);
+        }
+
         holder.imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -103,6 +125,7 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.ViewHolder> {
         ImageButton btnLike;
         ImageButton btnMore;
         ImageView ivUserProfile;
+        Switch stPublic;
 
         public ViewHolder(final View itemView) {
             super(itemView);
@@ -111,6 +134,7 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.ViewHolder> {
             btnLike = (ImageButton) itemView.findViewById(R.id.btnLike);
             btnMore = (ImageButton) itemView.findViewById(R.id.btnMore);
             ivUserProfile = (ImageView) itemView.findViewById(R.id.ivUserProfile);
+            stPublic = (Switch) itemView.findViewById(R.id.switcher_public);
         }
 
     }
@@ -119,6 +143,8 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.ViewHolder> {
         void onCommentsClick(View v, int position);
 
         void onMoreClick(View v, int position);
+
+        void onPublicClick(View v, int position, boolean b);
 
         void onProfileClick(View v);
     }
