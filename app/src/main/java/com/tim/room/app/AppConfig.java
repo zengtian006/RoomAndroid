3919ba33570a6c9f1d46ad5fe1905ab679295693
zengtim <zengtian006@gmail.com>
2017-03-09 19:44:53 -0800

    tim

--- app/src/main/java/com/tim/room/app/AppConfig.java
+++ app/src/main/java/com/tim/room/app/AppConfig.java
@@ -2,7 +2,8 @@ package com.tim.room.app;
 
 public class AppConfig {
 //    public static final String WS_BASE_URL = "http://192.168.1.102:62550/RoomBackendService/RoomBackend/";
-    public static final String WS_BASE_URL = "http://10.0.3.106:8080/RoomBackendService//RoomBackend/";
+//    public static final String WS_BASE_URL = "http://10.0.3.106:8080/RoomBackendService/RoomBackend/";
+    public static final String WS_BASE_URL = "http://119.23.142.252:8080/RoomBackendService/RoomBackend/";
 
 
     public static String IMG_BASE_URL = "http://tim198789.oss-cn-shanghai.aliyuncs.com/";
