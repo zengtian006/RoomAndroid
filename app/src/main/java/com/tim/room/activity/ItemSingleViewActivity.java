db56ce10dffd92d97467962bb50a93157feed34d
zengtim <zengtian006@gmail.com>
2017-03-10 17:10:10 -0800

    tim

--- app/src/main/java/com/tim/room/activity/ItemSingleViewActivity.java
+++ app/src/main/java/com/tim/room/activity/ItemSingleViewActivity.java
@@ -1,9 +1,12 @@
 package com.tim.room.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
@@ -11,19 +14,27 @@ import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextSwitcher;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.tim.room.R;
 import com.tim.room.model.Items;
+import com.tim.room.rest.RESTFulService;
+import com.tim.room.rest.RESTFulServiceImp;
 import com.tim.room.view.TagContainerLayout;
 
 import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 import static com.tim.room.MainActivity.session;
 import static com.tim.room.app.AppConfig.IMG_BASE_URL;
 
 public class ItemSingleViewActivity extends AppCompatActivity {
+    private static final String TAG = ItemSingleViewActivity.class.getSimpleName();
 
     Items mItem;
     ImageView ivFeedCenter;
@@ -38,6 +49,7 @@ public class ItemSingleViewActivity extends AppCompatActivity {
     FrameLayout vImageRoot;
     View vBgLike;
     ImageView ivLike;
+    RESTFulService updateItemService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,8 +61,56 @@ public class ItemSingleViewActivity extends AppCompatActivity {
         Bundle bundle = intent.getExtras();
         mItem = (Items) bundle.getSerializable("items");
 
+        updateItemService = RESTFulServiceImp.createService(RESTFulService.class);
+
         findView();
         setView();
+        setListener();
+    }
+
+    private void setListener() {
+        stPublic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final boolean isChecked = stPublic.isChecked();
+                String message = "";
+                String resultMsg = "";
+                if (isChecked) {
+                    message = "Do you want to set this item as public?";
+                    resultMsg = "This item has been set as public";
+                    mItem.setGlobal("1");
+                } else {
+                    message = "Do you want to set this item as private?";
+                    resultMsg = "This item has been set as private";
+                    mItem.setGlobal("0");
+                }
+                AlertDialog.Builder builder = new AlertDialog.Builder(ItemSingleViewActivity.this);
+                builder.setTitle("Room");
+                builder.setMessage(message);
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        stPublic.setChecked(!isChecked);
+                    }
+                });
+                final String finalResultMsg = resultMsg;
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        updateItemService.updateItem(mItem).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<Boolean>() {
+                            @Override
+                            public void accept(Boolean aBoolean) throws Exception {
+                                Log.v(TAG, "result: " + aBoolean);
+                                if (aBoolean) {
+                                    Toast.makeText(ItemSingleViewActivity.this, finalResultMsg, Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                });
+                builder.show();
+            }
+        });
     }
 
     private void setView() {
