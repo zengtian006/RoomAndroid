54db2708082ac8c9a560f4df8752796be300a088
zengtim <zengtian006@gmail.com>
2017-03-24 12:21:52 +0800

    tim

--- app/src/main/java/com/tim/room/fragment/MoreFragment.java
+++ app/src/main/java/com/tim/room/fragment/MoreFragment.java
@@ -43,12 +43,14 @@ public class MoreFragment extends Fragment {
 
     private void setView() {
         List<String> test = new ArrayList<>();
+        test.add("排行榜");
         test.add("设计师推荐搭配");
         test.add("每日最佳搭配");
 //        test.add("服装师问答");
         test.add("旧衣回收");
 
         List<Integer> testimage = new ArrayList<>();
+        testimage.add(R.drawable.leaderboard);
         testimage.add(R.drawable.designer);
         testimage.add(R.drawable.collocation);
 //        testimage.add(R.drawable.question);
